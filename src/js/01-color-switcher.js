d3f0c49d43013dbf9b53b0969aa8d10e264b99e9
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-10-11 22:49:43 +0200

    Update 01-color-switcher.js
    add\color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
 
+// Отримуємо елементи інтерфейсу
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+// Створюємо змінну для ідентифікатора інтервалу
+let intervalId = null;
+
+// Функція для генерування випадкового кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+// Функція для запуску зміни кольору
+function startChangeColor() {
+  // Запускаємо інтервал з кроком в одну секунду
+  intervalId = setInterval(() => {
+    // Змінюємо колір фону на випадкове значення
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  // Робимо кнопку "Start" неактивною
+  startButton.disabled = true;
+}
+
+// Функція для зупинки зміни кольору
+function stopChangeColor() {
+  // Зупиняємо інтервал
+  clearInterval(intervalId);
+  intervalId = null;
+
+  // Робимо кнопку "Start" активною
+  startButton.disabled = false;
+}
+
+// Додаємо слухача події на кнопку "Start"
+startButton.addEventListener('click', startChangeColor);
+
+// Додаємо слухача події на кнопку "Stop"
+stopButton.addEventListener('click', stopChangeColor);
